f3f0ee445f81c5050d976973ad6930d9dff00b20
Bobby Youdale <5367141+rustymagnet3000@users.noreply.github.com>
2021-06-08 09:38:52 +0100

    Added iac scan script and file to Dockerfile

---
+++ scripts/iac_scan.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -z "${SNYK_TOKEN}" ]; then
+  echo "[!]Please set SNYK_TOKEN variable in CircleCI project settings"
+  exit 1
+fi
+
+if [ -z "${GITHUB_SNYK_TOKEN}" ]; then
+  echo "[!]Please set GITHUB_SNYK_TOKEN variable in CircleCI project settings"
+  exit 1
+fi
+
+## set threshold to "high" as critical not yet supported in Snyk iac
+export SEVERITY_THRESHOLD=${SNYK_SEVERITY_THRESHOLD:="high"}
+
+TAG_NAME=${CONTAINER_TAG:="latest"}
+
+parse_and_post_comment () {
+  scan_results=$(parse_scan_results $1)
+  if [[ $scan_results ]]; then
+      comment_on_pr "$scan_results"
+  else
+    echo "No scan results found in $1."
+  fi
+}
+
+## auth
+snyk auth ${SNYK_TOKEN}
+
+## set organisation
+snyk config set org=${SNYK_ORG}
+
+## set project path
+PROJECT_PATH=$(eval echo ${CIRCLE_WORKING_DIRECTORY})
+
+## set tag
+SNYK_FNAME=snyk.json
+
+## lets retag the image
+docker image tag ${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1} ${CIRCLE_PROJECT_REPONAME}:${TAG_NAME}
+
+## test 
+snyk iac test --severity-threshold=${SEVERITY_THRESHOLD} --json > "${PROJECT_PATH}/${SNYK_FNAME}"
+
+echo "[*] Finished snyk IAC test"
+
+## parse results and check if we should comment back to GitHub
+if [[ -z "${CIRCLE_PULL_REQUEST}" ]]; then
+  echo "Not a pull request. Exiting"
+else
+  parse_and_post_comment "${PROJECT_PATH}/${SNYK_FNAME}"
+fi
